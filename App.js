99ad1660300cbc99a2f8a4da0cf9472d4df71e6a
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-10-12 11:27:56 -0700

    Create basic user login form

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [hasRegistered, setHasRegistered] = useState(false);
+
+  const handleLogin = (e) => {
+    setHasRegistered(!hasRegistered);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Fitness Monkey</Text>
+      <TextInput style={styles.textInput} placeholder="username" value={username} onChangeText={setUsername} />
+      <TextInput style={styles.textInput} placeholder="password" value={password} onChangeText={setPassword} />
+      <Button onPress={handleLogin} title={hasRegistered ? 'Login' : 'Register'} />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +29,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    borderColor: '#bbb',
+    borderWidth: 1,
+    margin: 10,
+    padding: 10,
+  }
 });
+
+export default App;
