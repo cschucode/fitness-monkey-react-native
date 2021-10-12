62abe522f0284cad0a6722da76ac7bf67fee784c
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-10-12 11:38:42 -0700

    Add navigation dependencies

--- App.js
+++ App.js
@@ -2,23 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+
 const App = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [hasRegistered, setHasRegistered] = useState(false);
 
-  const handleLogin = (e) => {
+  const handleAuth = (e) => {
+    // Register
+      // Create a user in the database
+      // Take user to registration form page
+    // Login
+      // Not a member, take them to registration page
+      // Are a member, take them to recovery dashboard
     setHasRegistered(!hasRegistered);
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Fitness Monkey</Text>
-      <TextInput style={styles.textInput} placeholder="username" value={username} onChangeText={setUsername} />
-      <TextInput style={styles.textInput} placeholder="password" value={password} onChangeText={setPassword} />
-      <Button onPress={handleLogin} title={hasRegistered ? 'Login' : 'Register'} />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Fitness Monkey</Text>
+        <TextInput style={styles.textInput} placeholder="username" value={username} onChangeText={setUsername} />
+        <TextInput style={styles.textInput} placeholder="password" value={password} onChangeText={setPassword} />
+        <Button onPress={handleAuth} title={hasRegistered ? 'Login' : 'Register'} />
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+
   );
 }
 
