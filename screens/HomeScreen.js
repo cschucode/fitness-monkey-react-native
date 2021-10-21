191482ccb0fba2d05dde526b78dad26e2527c612
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-10-21 09:25:20 -0700

    style home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import { Text, View, Button, StyleSheet, ScrollView } from 'react-native';
 
 const HomeScreen = ({ navigation, userInfo }) => {
   const { username, addiction, recoveryTime } = userInfo;
   const { days, hours, minutes } = recoveryTime;
 
+  const greeting = (name) => {
+    const time = new Date().getHours();
+    let timeOfDay = 'morning';
+
+    if (time < 4 && time > 19) {
+      timeOfDay = 'evening';
+    } else if (time > 11 && time < 20) {
+      timeOfDay = 'afternoon';
+    }
+
+    return `Good ${timeOfDay} ${name}`;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello {username}!</Text>
-      <Text style={styles.text}>Congratulations on</Text>
+    <ScrollView style={styles.container}>
+      <Text style={styles.greeting}>{greeting(username)}</Text>
+      <Text style={styles.greeting}>Take a moment to appreciate...</Text>
       <View style={styles.metrics}>
         <Text style={styles.metricsText}>{days} days</Text>
         <Text style={styles.metricsText}>{hours} hours</Text>
         <Text style={styles.metricsText}>{minutes} minutes</Text>
+        <Text style={styles.greeting}>without</Text>
+        <Text style={styles.greeting}>{addiction}</Text>
       </View>
-      <Text style={styles.text}>free from {addiction}!</Text>
-    </View>
+
+    </ScrollView>
   );
 };
 
@@ -24,13 +39,23 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
+  },
+  greeting: {
+    fontSize: 18,
+    color: 'orange',
+    marginVertical: 10,
   },
   metrics: {
     borderColor: 'dodgerblue',
-    borderWidth: 1,
+    borderRadius: 150,
+    borderWidth: 3,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     padding: 50,
+    height: 300,
+    width: 300,
+    margin: 30,
   },
   metricsText: {
     color: 'orange',
